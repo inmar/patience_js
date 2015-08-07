798a3d18900a0cf6010cace14491154a543e302d
Afaq Tariq <atariq@inmar.com>
2015-08-07 14:24:41 -0400

    added qretry to lessen complexity. refactored flow added to auto-retry

--- src/auto-retry.js
+++ src/auto-retry.js
@@ -2,141 +2,124 @@
 
   'use strict';
 
-  /**
-   * POJO of retry globals and functions.
-   * @type {Object}
-   */
-  var retry = {
-    defaults : {
-      max: 3, // number of times to retry request
-      interval: 50, // ms
-      failTimeout: 5000, //ms
-      reAttemptOnFailure: true,
-      maxReAttempts: 2
-    },
-    parseConfig: function (providedConfig) {
-      if (!providedConfig || Object.keys(providedConfig).length === 0) {
-        return this.defaults;
-      } else {
-        return this.overrideRetryDefaults(providedConfig);
+  var blockedUrls = [];
+  var requestsBlocked = false;
+  var options = {
+    defaults: {
+      retry: {
+        max: 2,
+        interval: 100,
+        intervalMultiplicator: 1,
+      },
+      reAttempt: {
+        max: 2,
+        interval: 1000,
+        intervalMultiplicator: 1,
       }
     },
-    overrideRetryDefaults: function (providedConfig) {
+    override: function (options, defaultOptions) {
       var resultConfig = {};
-      var retry        = this;
 
-      Object.keys(this.defaults).map(function (key) {
-        resultConfig[key] = providedConfig[key] || retry.defaults[key];
+      Object.keys(defaultOptions).map(function (key) {
+        resultConfig[key] = options[key] || defaultOptions[key];
       });
 
       return resultConfig;
     },
-    initiate: function (providedRequestConfig, providedRetryConfig, $httpLibrary, $promiseLibrary) {
-
-      var httpConfig  = request.parseConfig(providedRequestConfig);
-
-      if (request.isBlocked) {
-        httpConfig.response.reject({ msg: 'Cannot process request at this time. Please wait or refresh browser and try again.' });
+    parse: function (options, defaultOptions) {
+      if (!options || Object.keys(options).length === 0) {
+        return defaultOptions;
       } else {
-        request.attempt(httpConfig, providedRetryConfig);
+        return this.override(options, defaultOptions);
       }
-
-      return httpConfig.response.promise;
     }
   };
-
-  /**
-   * POJO of request-based logic and globals.
-   * @type {Object}
-   */
-  var request = {
-    isBlocked: false,
-    attempt: function (httpConfig, providedRetryConfig) {
-
-      var req = this;
-      var retryConfig = retry.parseConfig(providedRetryConfig);
-
-      axios(httpConfig).then(function (res) {
-
-        httpConfig.response.resolve(res);
-
-      }).catch(function (err) {
-
-        // increment http attempt counter for this request
-        httpConfig.attempts++;
-
-        if (httpConfig.attempts >= retryConfig.max) {
-
-          var rejectionMessage = 'Max retries exhausted.';
-
-          // retries have failed, begin re-attempts
-          if (retryConfig.reAttemptOnFailure) {
-
-            req.setUpReAttemptInterval(httpConfig, retryConfig);
-
-            // set a flag to block future requests
-            req.isBlocked = true;
-
-            httpConfig.response.notify();
-          } else {
-            httpConfig.response.reject(err);
-          }
-
-        } else {
-          httpConfig.response.notify(httpConfig.attempts);
-          req.attempt(httpConfig, retryConfig);
-        }
-
-      });
-    },
-    parseConfig: function (httpConfig) {
-      httpConfig.response = Q.defer();
-      httpConfig.attempts = 0;
-      httpConfig._reAttempts = 0;
-      return httpConfig;
-    },
-    reAttempt: function () {
-
-      axios(httpConfig).then(function (res) {
-        PubSub.publish('reAttemptSuccessful', {msg: 'Re-attempt was successful.'});
-
-        // ISSUE: how to let the application know that this occurred?
-        httpConfig.response.resolve(res);
-
-      }).catch(function () {
-        httpConfig._reAttempts++;
-        self.reAttempt(httpConfig, interval, maxReAttempts)
-        return;
-      });
-
-    },
-    setUpReAttemptInterval: function reAttempt (httpConfig, retryConfig) {
-
-
-      if (httpConfig._reAttempts >= retryConfig.maxReAttempts) {
-        httpConfig.response.reject({ msg: 'Max re-attempts reached.'});
-        return;
-      };
-
-      setTimeout(function () {
-
-        axios(httpConfig).then(function (res) {
-          PubSub.publish('reAttemptSuccessful', {msg: 'Re-attempt was successful.'});
-          httpConfig.response.resolve(res);
-        }).catch(function () {
-          httpConfig._reAttempts++;
-          reAttempt(httpConfig, retryConfig);
-        });
-
-      }, retryConfig.failTimeout);
-
-    },
+  var messages = {
+    retryFailed: 'Request failed.',
+    reAttemptsFailed: 'Re-attempt of request failed.',
+    requestsBlocked: 'Requests are currently blocked by Retry library.',
   };
 
   var AjaxRetry = function (providedRequestConfig, providedRetryConfig) {
 
-    return retry.initiate(providedRequestConfig, providedRetryConfig);
+    return {
+      _doRetry: function (response) {
+        this._retryParams.maxRetry = this._retryParams.max;
+        var self = this;
 
+        // Retry
+        Qretry(function () {
+
+          return axios(self._requestParams);
+
+        }, self._retryParams).then(function (res) {
+
+          response.resolve(res);
+
+        }).catch(function () {
+
+          // block future calls
+          requestsBlocked = true;
+
+          PubSub.publish('retriesFailed', messages.retryFailed);
+          response.notify(messages.retryFailed);
+
+          self._doReAttempt(response);
+
+        });
+
+      },
+      _doReAttempt: function (response) {
+
+        this._reAttemptParams.maxRetry = this._reAttemptParams.max - 1;
+        var self = this;
+
+        // Re-attempt
+        Qretry(function () {
+
+          return axios(self._requestParams);
+
+        }, self._reAttemptParams).then(function (res) {
+
+          requestsBlocked = false;
+          response.resolve(res);
+
+        }).catch(function (err) {
+
+          requestsBlocked = false;
+          PubSub.publish('reAttemptsFailed', messages.reAttemptsFailed);
+          response.reject(messages.reAttemptsFailed);
+
+        });
+
+      },
+      retry: function (params) {
+        this._retryParams = options.parse(params, options.defaults.retry);
+        return this;
+      },
+      request: function (params) {
+        this._requestParams = params;
+        return this;
+      },
+      reAttempt: function (params) {
+        this._reAttemptParams = options.parse(params, options.defaults.reAttempt);
+        return this;
+      },
+      run: function () {
+
+        var response = Q.defer();
+
+        if (requestsBlocked) {
+
+          response.reject(messages.requestsBlocked);
+
+        } else {
+          this._doRetry(response);
+        }
+
+        return response.promise;
+      }
+    };
   };
 
   (function(name, obj) {
